726ba347abf7e2daaebfb662e53f6941d3efdec7
bettercallhunter <huntershen10@gmail.com>
2023-01-18 12:18:28 -0500

    add ff

---
+++ sbrk.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main() {
+    int* x;
+    int* y;
+    int* z;
+    int* o;
+    o = sbrk(0);
+    printf("initialization is %p \n", (o));
+    x = sbrk(sizeof(int));
+    y = sbrk(sizeof(int));
+    z = sbrk(sizeof(int));
+    *x = 1;
+    *y = 2;
+    *z = 3;
+    printf("x = %p,y =c %p,z = %p \n", x, y, z);
+    printf("now freeing\n");
+    void* j = sbrk(-4);
+    printf("j = %p", j);
+    printf("\n **************\n");
+    void* k = sbrk(0);
+    printf("k = %p", k);
+    return 1;
+}
