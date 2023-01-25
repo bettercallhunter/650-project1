ffd4f68156cf880e994688e39a4ba451dfb29932
Hunter <zs149@duke.edu>
2023-01-25 11:14:18 -0500

    try bf

--- my_malloc/my_malloc.c
+++ my_malloc/my_malloc.c
@@ -9,47 +9,47 @@
 #include <unistd.h>
 unsigned long heap_size = 0;
 void *ff_malloc(size_t size) {
-    Node *curr = head;
-    while (curr != NULL) {
-        // find the next Node that is at least larger than size
-        if (curr->size >= size) {
-            break;
-        } else {
-            // traversal through the linkedlist
-            curr = curr->next;
-        }
-    }
-    // if no Node satisfied found, allocate new space
-    if (curr == NULL) {
-        void *ptr = sbrk(Meta_size + size);
-        heap_size += Meta_size + size;
-        if (ptr == (void *)-1) {
-            return NULL;
-        }
-        Node *requestedSpace = (Node *)ptr;
-        requestedSpace->prev = NULL;
-        requestedSpace->next = NULL;
-        requestedSpace->size = size;
-        // // add a offset of size of Meta data
-        return (void *)requestedSpace + Meta_size;
-    }
-    // enough space to split : size > meta data and allocation size
-    if (curr->size > size + Meta_size) {
-        Node *allocatedSpace = (Node *)((void *)curr + (curr->size) - size);
-        curr->size -= Meta_size + size;
-        allocatedSpace->size = size;
-        allocatedSpace->prev = NULL;
-        allocatedSpace->next = NULL;
-        return (void *)allocatedSpace + Meta_size;
-    }
-    // space enough to allocate, but not enough to hold meta data, no split needed, remove node from the linkedlist
-    else if (curr->size >= size && curr->size - size <= Meta_size) {
-        removeNode(curr);
-        return (void *)curr + Meta_size;
-    }
+    // Node *curr = head;
+    // while (curr != NULL) {
+    //     // find the next Node that is at least larger than size
+    //     if (curr->size >= size) {
+    //         break;
+    //     } else {
+    //         // traversal through the linkedlist
+    //         curr = curr->next;
+    //     }
+    // }
+    // // if no Node satisfied found, allocate new space
+    // if (curr == NULL) {
+    //     void *ptr = sbrk(Meta_size + size);
+    //     heap_size += Meta_size + size;
+    //     if (ptr == (void *)-1) {
+    //         return NULL;
+    //     }
+    //     Node *requestedSpace = (Node *)ptr;
+    //     requestedSpace->prev = NULL;
+    //     requestedSpace->next = NULL;
+    //     requestedSpace->size = size;
+    //     // // add a offset of size of Meta data
+    //     return (void *)requestedSpace + Meta_size;
+    // }
+    // // enough space to split : size > meta data and allocation size
+    // if (curr->size > size + Meta_size) {
+    //     Node *allocatedSpace = (Node *)((void *)curr + (curr->size) - size);
+    //     curr->size -= Meta_size + size;
+    //     allocatedSpace->size = size;
+    //     allocatedSpace->prev = NULL;
+    //     allocatedSpace->next = NULL;
+    //     return (void *)allocatedSpace + Meta_size;
+    // }
+    // // space enough to allocate, but not enough to hold meta data, no split needed, remove node from the linkedlist
+    // else if (curr->size >= size && curr->size - size <= Meta_size) {
+    //     removeNode(curr);
+    //     return (void *)curr + Meta_size;
+    // }
 }
 void *bf_malloc(size_t size) {
-    /*
+    
     Node *curr = head;
     Node *best = NULL;
     size_t overHead = SIZE_MAX;
@@ -96,14 +96,14 @@ void *bf_malloc(size_t size) {
         removeNode(curr);
         return (void *)curr + Meta_size;
     }
-    */
+    
 }
 
 void ff_free(void *ptr) {
-    my_free(ptr);
+    //my_free(ptr);
 }
 void bf_free(void *ptr) {
-    // my_free(ptr);
+    my_free(ptr);
 }
 void merge(Node *toAdd) {
     mergeBack(toAdd);
